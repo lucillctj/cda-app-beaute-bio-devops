c58a7aa8222529f494ec9f7bc4b40d41c31e80b8
Lucille C <114987982+lucillctj@users.noreply.github.com>
2023-04-12 10:58:59 +0200

    add template engine

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
+import path from "path";
 
+const __dirname = path.resolve();
+
+// create express app
 const app = express();
+
+// set views engine
+app.set("view engine", "ejs");
+
+//set views directory
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname,
+    "public")));
 
 app.get("/", (req, res) => {
     res.send("Hello world!")
 });
 
+//create server and listenning
 app.listen(8082, () => {
     console.log("Server is listenning at port 8082!")
 })
