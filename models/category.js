b2a58df2aafabd9250a6f4f7502cbc71c8362188
Lucille C <114987982+lucillctj@users.noreply.github.com>
2023-04-12 15:31:32 +0200

    add category schema

---
+++ models/category.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const {Schema} = mongoose;
+
+const categorySchema = new Schema({
+    categoryName: {
+        type: String,
+        require: true
+    }
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
