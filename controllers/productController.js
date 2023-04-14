d1467d169aec619c311781407ea2cb24b66c1db1
Lucille C <114987982+lucillctj@users.noreply.github.com>
2023-04-14 10:13:38 +0200

    add endpoints and routes to get products depending category

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,18 +3,42 @@ import Product from "../models/product.js";
 //get all products
 export const getProducts = async (req, res, next) => {
     const products = await Product.find();
-
+    
     console.log(products);
     res.status(200).json({products});
 };
 
 //get product by id
 export const getProductById = async (req, res, next) => {
-    const productId = req.body.id
+    const productId = req.body.id;
     const product = await Product.findById({_id: productId});
-
+    
     console.log(product);
     res.status(200).json({product});
+};
+
+export const getProductsSoinsDuCorps = async (req, res, next) => {
+    const products = await Product.find({categoryId: '6437b43e926978d81fcf6819'});
+    console.log(products);
+    res.status(200).json({products});
+};
+
+export const getProductsSoinsDuVisage = async (req, res, next) => {
+    const products = await Product.find({categoryId: '6437b456926978d81fcf681b'});
+    console.log(products);
+    res.status(200).json({products});
+};
+
+export const getProductsMaquillage = async (req, res, next) => {
+    const products = await Product.find({categoryId: '6436c5f0a6e9ad5ceea19171'});
+    console.log(products);
+    res.status(200).json({products});
+};
+
+export const getProductsBeauteDesMains = async (req, res, next) => {
+    const products = await Product.find({categoryId: '6437ba8dc561d8ee4795b256'});
+    console.log(products);
+    res.status(200).json({products});
 };
 
 //create product
@@ -27,9 +51,8 @@ export const postProduct = async (req, res, next) => {
     };
     const product = await Product.create(newProduct);
     console.log(product);
-
     res.status(201).json({product});
-}
+};
 
 //update product
 export const updateProduct = async (req, res, next) => {
@@ -43,7 +66,6 @@ export const updateProduct = async (req, res, next) => {
     await Product.findByIdAndUpdate({ _id: productId}, updateProduct, {new: true});
     res.status(200).json({message: `product n° ${productId} is updated!`});
 };
-
 
 //delete product
 export const deleteProduct = async (req, res, next) => {
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { deleteProduct, updateProduct, getProducts, getProductById, postProduct } from "../controllers/productController.js";
+import { deleteProduct, updateProduct, getProducts, getProductById, postProduct, getProductsSoinsDuCorps, getProductsSoinsDuVisage, getProductsMaquillage, getProductsBeauteDesMains } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/product", getProductById);
+router.get("/products/soins-du-corps", getProductsSoinsDuCorps);
+router.get("/products/soins-du-visage", getProductsSoinsDuVisage);
+router.get("/products/maquillage", getProductsMaquillage);
+router.get("/products/beaute-des-mains", getProductsBeauteDesMains);
+
 router.post("/add-product", postProduct);
 router.put("/product", updateProduct);
 router.delete("/product", deleteProduct);
